976c9dda683b11c55a90be53ae19413cda918105
taya23kot <61755758+taya23kot@users.noreply.github.com>
2020-12-27 17:47:12 +0100

    total update

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/* MENU BUTTONS BEGIN */
+
+document.getElementById("open-menu").onclick=myfunction();
+
+function myfunction (){
+    document.getElementById("dropdown").style.display = "block";
+}
+
+document.getElementById("close-menu").onclick=closingFunction();
+
+function closingFunction (){
+    document.getElementById("dropdown").style.display = "none";
+}
+
+document.getElementsByClassName("menu-link-project").onclick=closingFunction();
+
+document.getElementsByClassName("menu-link-org").onclick=closingFunction();
+
+/* MENU BUTTONS END */
+
+document.getElementById("scroll").onclick=scrollFunction();
+
+function scrollFunction (){
+    let scrollDiv = document.getElementById("about-project").offsetTop;
+    window.scrollTo({ top: scrollDiv, behavior: 'smooth'});
+}
